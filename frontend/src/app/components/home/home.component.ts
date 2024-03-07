353a495b4aa454c5a2d95578730307df1d09d910
Ali Ozen <a.ozen@eloaded.eu>
2024-03-07 10:42:24 +0100

    added update pokemon formgroup

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+
+  constructor(private http: HttpClient) {}
+
+  updatePokemonsForm = new FormGroup({
+    generation: new FormControl('', [
+      Validators.required
+    ])
+  })
+
+  onSubmit() {
+    this.http
+      .post<any>('http://localhost:3000/poke/update', this.updatePokemonsForm.value)
+      .subscribe({
+        next: (resp) => {
+          console.log(resp);
+          alert('updated');
+        },
+        error: (err) => {
+          console.log(err);
+          alert('not updated');
+        },
+      });
+  }
 
 }
